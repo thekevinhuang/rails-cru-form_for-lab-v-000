8ec88dec59c19ef4303fea9bdff26efed19c1872
Kevin Huang <yellow.khaos@gmail.com>
2018-06-15 04:09:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,7 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
     @genre = Genre.find(@song.genre_id)
-    @artist = Genre.find(@song.artist_id)
+    @artist = Artist.find(@song.artist_id)
   end
 
   def new
